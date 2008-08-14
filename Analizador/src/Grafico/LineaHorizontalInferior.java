5f4a80a4668233472dcea668d8d9572f50fe9f6c
guillevene <guillevene@f4230cc4-4854-0410-9679-69ab71b14cf8>
2008-08-14 02:25:11 +0000

    Comenzando con el dibujo de figuras. Todavia no anda nada. Quizas hay metodos que no se usen. Seguro hay metodos que faltan.

---
+++ Analizador/src/Grafico/LineaHorizontalInferior.java
@@ -0,0 +1,21 @@
+package Grafico;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+public class LineaHorizontalInferior extends Grafico{
+    
+     public LineaHorizontalInferior (Integer x0, Integer y0, Integer w0, Integer h0, Color c, JPanel p) {
+        super(x0, y0,w0,h0,c,p);
+     }
+        
+    @Override
+    public void dibujate(Graphics2D g2) {
+        g2.setColor(color);
+        g2.fillRect(x, y, w, h);
+    }
+}
+
+
+    
