f3b9c90e5e4200d32d58befee827ad553a179406
guillevene <guillevene@f4230cc4-4854-0410-9679-69ab71b14cf8>
2008-08-16 19:46:10 +0000

    Ya grafica.

--- Analizador/src/analizador/Dibujo.java
+++ Analizador/src/analizador/Dibujo.java
@@ -8,7 +8,10 @@ package analizador;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Line2D;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 /**
  *
@@ -16,9 +19,18 @@ import javax.swing.JPanel;
  */
 public class Dibujo extends JPanel{
     
-    int[] muestras;
-    Color color;
-    String nombre;
+    int[] muestras = {1,0,1,0};
+    Color color = Color.RED;
+    String nombre = "Canal1";
+    int ancho_bit = 25; // this.getWidth() / muestras.length;
+    int altura_bit = 50;
+    int altura_eje = 60;
+    int ancho_eje = 60;
+    
+    public Dibujo(){
+        super();
+        this.setBackground(Color.BLACK);
+    }
     
     public void cambiarRango(int[] muestras){
         this.muestras = muestras;
@@ -43,14 +55,25 @@ public class Dibujo extends JPanel{
     public void paintComponent(Graphics g){
         super.paintComponent (g);
         Graphics2D g2 = (Graphics2D)g;
-        int width_bit = 20; // this.getWidth() / muestras.length;
-        int heigth_bit = 20; 
-        g2.drawString(nombre,20,this.getHeight()/2);
+        
+        g2.setColor(color);
+        g2.drawString(nombre,5,altura_eje-(altura_bit/2));
         for (int i = 0; i<muestras.length;i++){
-            if (i>0 && muestras[i]!=muestras[i-1]) /* Si hay un cambio de signo.. */  
-                    g2.drawLine(width_bit*i, heigth_bit ,width_bit*(i+1),0); /* dibujo una linea vertical antes de.. */
-            g2.drawLine(width_bit*i,muestras[i]*heigth_bit ,width_bit*(i+1),muestras[i]*heigth_bit); /* dibujar la proxima linea horizontal*/
+            System.out.println(muestras[i]);
+            if (i>0 && muestras[i]!=muestras[i-1]){ // Si hay un cambio de signo..   
+                g2.drawLine(ancho_eje + ancho_bit*i, altura_eje - altura_bit ,ancho_eje+ancho_bit*i, altura_eje); // dibujo una linea vertical antes de..
+            }
+            g2.drawLine(ancho_eje+ancho_bit*i,altura_eje - muestras[i]*altura_bit ,ancho_eje+ancho_bit*(i+1), altura_eje - muestras[i]*altura_bit); // dibujar la proxima linea horizontal
         }
-    }
+        /*Graphics2D g2 = (Graphics2D) g;
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 
+    g2.setPaint(Color.gray);
+    int x = 5;
+    int y = 7;
+
+    g2.draw(new Line2D.Double(x, y, 200, 200));
+    g2.drawString("Line2D", x, 250);*/
+    }
+    
 }
