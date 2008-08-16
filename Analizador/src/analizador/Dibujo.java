61db40e5196d9ec2311454ea107e218c0ea818e9
guillevene <guillevene@f4230cc4-4854-0410-9679-69ab71b14cf8>
2008-08-16 21:24:30 +0000

    Ahora la señal toma el tamaño del Jframe.

--- Analizador/src/analizador/Dibujo.java
+++ Analizador/src/analizador/Dibujo.java
@@ -1,31 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package analizador;
 
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.geom.Line2D;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 
-/**
- *
- * @author Administrador
- */
 public class Dibujo extends JPanel{
     
     private int[] muestras = {1,0,1,0};
     private Color color = Color.RED;
     private String nombre = "Canal1";
-    private int ancho_bit = 25; // this.getWidth() / muestras.length;
-    private int altura_bit = 50;
-    private int altura_eje = 60;
-    private int ancho_eje = 60;
+    private int ancho_bit; 
+    private int altura_bit;
+    private int altura_eje;
+    private int ancho_eje;
     
     public Dibujo(){
         super();
@@ -51,11 +40,27 @@ public class Dibujo extends JPanel{
         repaint();
     }
     
+    private void setLayout1(){
+        ancho_bit = 25; 
+        altura_bit = 50;
+        altura_eje = 60;
+        ancho_eje = 60;
+    }
+    /* Estos metodo deben ejecutarse despues 
+     de que se inicialice la altura del Jpanel*/
+    private void setLayout2(){
+        ancho_bit = 25; 
+        altura_bit = this.getHeight()-10;
+        altura_eje = this.getHeight()-5;
+        ancho_eje = 60;
+    }
+    
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent (g);
         Graphics2D g2 = (Graphics2D)g;
-        
+        //setLayout1();
+        setLayout2();
         g2.setColor(color);
         g2.drawString(nombre,5,altura_eje-(altura_bit/2));
         for (int i = 0; i<muestras.length;i++){
@@ -65,15 +70,6 @@ public class Dibujo extends JPanel{
             }
             g2.drawLine(ancho_eje+ancho_bit*i,altura_eje - muestras[i]*altura_bit ,ancho_eje+ancho_bit*(i+1), altura_eje - muestras[i]*altura_bit); // dibujar la proxima linea horizontal
         }
-        /*Graphics2D g2 = (Graphics2D) g;
-    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-
-    g2.setPaint(Color.gray);
-    int x = 5;
-    int y = 7;
-
-    g2.draw(new Line2D.Double(x, y, 200, 200));
-    g2.drawString("Line2D", x, 250);*/
     }
     
 }
