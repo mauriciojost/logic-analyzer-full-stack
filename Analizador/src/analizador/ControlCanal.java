5e73cd4d1dea021a3994b991fbf7f5af737e6307
guillevene <guillevene@f4230cc4-4854-0410-9679-69ab71b14cf8>
2009-01-02 23:17:57 +0000

    ControlCanal implementa un MouseListener. Cuando se presiona el botón izquierdo del Mouse una ventana de entrada de texto aparece, con el nombre del canal como valor por defecto. Cuando se presiona el botón derecho del Mouse una paleta de colores aparece, el color elegido se aplica al color de fondo de ControlCanal y al color de la señal en Dibujo.
    Las siguientes modificaciones implican un cambio de diseño.
    Se agrega el método paintComponent que debe imprimir el nombre del Canal.
    Cuando se crea un ControlCanal, se inicializa su nombre, pidiendo el ID a Canal. 
    También la fuente utilizada para el nombre del Canal y el color de nombre.
    Se comenta el método cambiarNombre en ControlCanal.
    Se analiza la posibilidad de que ControlCanal herede de JLabel, debido a las posibilidades de centrado de texto.

--- Analizador/src/analizador/ControlCanal.java
+++ Analizador/src/analizador/ControlCanal.java
@@ -2,21 +2,33 @@
 package analizador;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Observable;
 import java.util.Observer;
+import javax.swing.JColorChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ControlCanal extends JPanel implements Observer{
+public class ControlCanal extends JPanel implements Observer, MouseListener{
 
     private String nombre;
     private Canal canal;
     private Dibujo dibujo;
     private Color colorFondo = new Color(90,90,90);
+    private Color colorNombre = new Color(204,204,204);
+    private Font fuente = new Font("Arial", 1 , 12);
      
     public ControlCanal(Canal canal,Dibujo dibujo){
         this.setBackground(colorFondo);
         this.canal = canal;
-        this.dibujo = dibujo;        
+        this.dibujo = dibujo;
+        this.nombre = "Canal "+ canal.obtenerId();
+        this.addMouseListener(this);
     }
     
     public void cambiarRango(int i, int f){
@@ -31,9 +43,9 @@ public class ControlCanal extends JPanel implements Observer{
         dibujo.cambiarColor(color);
     }
     
-    public void cambiarNombre(String nombre){
+    /*public void cambiarNombre(String nombre){
         dibujo.cambiarNombre(nombre);
-    }
+    }*/
     
     private void actualizarDibujo(){
         dibujo.actualizarDibujo();
@@ -42,5 +54,44 @@ public class ControlCanal extends JPanel implements Observer{
     public void update(Observable o, Object arg) {
         int[] rango = (int[])arg;
         cambiarRango(rango[0],rango[1]);
+    }
+
+    public void mouseClicked(MouseEvent e){
+        if (e.getButton()==e.BUTTON1){
+            String str = JOptionPane.showInputDialog(null, "Nombre de Canal", nombre);
+            if (str != null) {
+                this.nombre = str;
+                this.repaint();
+            }            
+        }
+        else if (e.getButton()==e.BUTTON3){
+            Color newColor = JColorChooser.showDialog(
+                null,"Elija un color",
+                this.getBackground());
+            if (newColor != null) {
+                this.setBackground(newColor);
+                this.cambiarColor(newColor);
+            }
+        }
+    }
+
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+
+    public void paintComponent(Graphics g){
+        try{
+            super.paintComponent (g);
+            Graphics2D g2 = (Graphics2D)g;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+            g2.setColor(colorNombre);
+            g2.setFont(fuente);
+            g2.drawString(nombre,32,23);
+
+        }catch(NullPointerException e){
+            System.out.println("Error - Parámetros no Inicializados");
+        }
     }
 }
