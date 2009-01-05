dd120396ad6db812b69ce94b1a0471d59c0c04b2
mauriciojost <mauriciojost@f4230cc4-4854-0410-9679-69ab71b14cf8>
2009-01-05 18:34:19 +0000

    Make de JNI con variable de entorno JDK.

--- Analizador/src/analizador/makeJNI.bat
+++ Analizador/src/analizador/makeJNI.bat
@@ -1,11 +1,14 @@
-@echo off
+echo @echo off
+echo Recuerde definir la variable de entorno JDK.
+echo Esta puede ser por ejemplo:
+echo    C:\Archivos de programa\Java\jdk1.6.0_01
 javac Comunicador.java
 cd ..
 javah -jni analizador.Comunicador
 move *.h .\analizador\
 cd analizador
 echo ml rutina.asm /c /coff
-cl -I"C:\Archivos de programa\Java\jdk1.6.0_01\include\win32" -I"C:\Archivos de programa\Java\jdk1.6.0_01\include" -LD Comunicado.cpp 
+cl -I"%JDK%\include\win32" -I"%JDK%\include" -LD Comunicado.cpp 
 echo rutina.obj
 copy Comunicado.dll c:\windows\system32\
 del *.class
