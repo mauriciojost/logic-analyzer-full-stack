c3cad46dc78920dae885bfaa5392dae986409e55
mauriciojost <mauriciojost@f4230cc4-4854-0410-9679-69ab71b14cf8>
2009-01-13 21:20:02 +0000

    Funcionando el Incomunicado.

--- Analizador/src/analizador/script.sh
+++ Analizador/src/analizador/script.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 echo "Compilando Comunicador.java..."
-echo "javac Comunicador.java"
+javac Comunicador.java
 cd ..
 echo "Generando el .h..."
-echo "javah -jni analizador.Comunicador"
-echo "mv *.h ./analizador/"
+javah -jni analizador.Comunicador
+mv *.h ./analizador/
 cd analizador
 echo "Compilando el .asm..."
-echo "ml rutina.asm /c /coff"
+echo "nasm rutina.asm /c /coff"
 echo "Compilando el .c..."
-gcc Comunicado.c -ocomunicado.so -Wl,-soname,libnative.so -I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0/include -I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0/include/linux -shared
-echo "Copiando .so al directorio misdoc..."
+gcc Incomunicado.c -ocomunicado.so -Wl,-soname,libnative.so -I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0/include -I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.0/include/linux -shared
+echo "Copiando .so al directorio /usr/lib/..."
 cp comunicado.so /usr/lib/comunicado.so
-cd ../../dist
 echo "Ejecutando el .jar..."
-java -jar Analizador.jar
+echo "java -jar ../../dist/Analizador.jar"
 echo "Volviendo a src..."
-cd ..
-cd src
-cd analizador
+
