57ef5220b5904a7eb1e20cc194a6df6274896975
mauriciojost <mauriciojost@f4230cc4-4854-0410-9679-69ab71b14cf8>
2009-02-05 21:52:08 +0000

    Agregando más cosas al generador de señales...

--- Senales_PIC/Main.c
+++ Senales_PIC/Main.c
@@ -3,7 +3,43 @@
 
 #fuses HS,NOWDT,NOPROTECT,PUT,NOLVP // Fuses, tipo de oscilador, WDT off, sin protección de código...
 #use delay(clock=12000000) // Clock de 12 MHz.
+
+
+//#define DELAY delay_ms(100);  // Sólo para probar GUILLE!
+#define DELAY;					// Este sirve para simular, y para el original...
+
 int8 i=0;
+
+
+
+
+
+void hacer_parpadeo(){
+	int8 i=0;
+	do{
+		i = ~i;
+		output_d(i);
+		DELAY
+	}while(1);
+}
+
+void hacer_shift(){
+	int8 i=1;
+	int8 j;
+	do{
+		for(j=0;j<8;j++){
+			i=i<<1;
+			output_d(i);
+			DELAY
+		}
+		for(j=0;j<8;j++){
+			i=i>>1;
+			output_d(i);
+			DELAY
+		}
+
+	}while(1);
+}
 
 
 
@@ -11,6 +47,7 @@ void hacer_decr1(){
 	int8 i;
 	do{
 		output_d(i--);
+		DELAY
 	}while(1);
 }
 
@@ -18,6 +55,7 @@ void hacer_cuenta1(){
 	int8 i;
 	do{
 		output_d(i++);
+		DELAY
 	}while(1);
 }
 
