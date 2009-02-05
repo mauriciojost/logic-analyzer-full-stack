db3da4f80b8275f3ee3e051cbf4a48e94ccc9c8e
mauriciojost <mauriciojost@f4230cc4-4854-0410-9679-69ab71b14cf8>
2009-02-05 23:02:00 +0000

    El cambio de señales del generador se hace con reset.

--- Senales_PIC/Main.c
+++ Senales_PIC/Main.c
@@ -1,5 +1,6 @@
 
 #include <16f877a.h> // Pic a utilizar.
+#include <stdlib.h>
 
 #fuses HS,NOWDT,NOPROTECT,PUT,NOLVP // Fuses, tipo de oscilador, WDT off, sin protección de código...
 #use delay(clock=12000000) // Clock de 12 MHz.
@@ -7,11 +8,9 @@
 
 //#define DELAY delay_ms(100);  // Sólo para probar GUILLE!
 #define DELAY;					// Este sirve para simular, y para el original...
+//#define DELAY delay_us(30);
 
-int8 i=0;
-
-
-
+int8 actividad=0;
 
 
 void hacer_parpadeo(){
@@ -27,21 +26,18 @@ void hacer_shift(){
 	int8 i=1;
 	int8 j;
 	do{
-		for(j=0;j<8;j++){
+		for(j=0;j<7;j++){
 			i=i<<1;
 			output_d(i);
 			DELAY
 		}
-		for(j=0;j<8;j++){
+		for(j=0;j<7;j++){
 			i=i>>1;
 			output_d(i);
 			DELAY
 		}
-
 	}while(1);
 }
-
-
 
 void hacer_decr1(){
 	int8 i;
@@ -59,29 +55,50 @@ void hacer_cuenta1(){
 	}while(1);
 }
 
-
-
-// Función que trata a la interrupción producida por un flanco en la pata INT.
-#INT_EXT
-void interrupcion_int(){
-	
-	clear_interrupt(INT_EXT);
-	i++;
-	switch(i){
-		case 0: hacer_cuenta1(); break;
-		case 1: hacer_decr1(); break;
-		case 2: hacer_cuenta1(); break;
-		case 3: hacer_cuenta1(); break;
-		case 4: hacer_cuenta1(); break;
-	}
+void hacer_cuenta2(){
+	int8 i;
+	do{
+		output_d(i++);
+		delay_us(100);
+	}while(1);
 }
 
+void hacer_cuenta3(){
+	int8 i;
+	do{
+		output_d(i++);
+		delay_us(200);
+	}while(1);
+}
 
-
+void hacer_aleat(){
+	int8 i;
+	do{
+		i=rand();
+		output_d(i);
+		
+	}while(1);
+}
 
 void main(){
+
 	set_tris_d(0x00);
-	enable_interrupts(INT_EXT); // Configuración de las interrupciones.
-	enable_interrupts(GLOBAL);	
-	while(1){}
+
+	actividad = (read_eeprom(0x0)+1); 
+	actividad = (actividad>8)?0:actividad;
+
+	write_eeprom(0x0,actividad);
+	switch(actividad){
+		case 0: hacer_cuenta1(); break;
+		case 1: hacer_decr1(); break;
+		case 2: hacer_parpadeo(); break;
+		case 3: hacer_shift(); break;
+		case 4: hacer_cuenta2(); break;
+		case 5: hacer_cuenta3(); break;
+		case 6: hacer_aleat(); break;
+		case 7: hacer_cuenta1(); break;
+		default: break;
+	}
+
+	
 }
